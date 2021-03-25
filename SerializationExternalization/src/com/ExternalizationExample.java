b058b99dd81851cfd5905dbe7f681e24d6af095a
Mayar <mdmayaralam25@gmail.com>
2021-03-25 10:28:03 +0530

    Corrupted data case for Externalization Example.

--- SerializationExternalization/src/com/ExternalizationExample.java
+++ SerializationExternalization/src/com/ExternalizationExample.java
@@ -43,6 +43,13 @@ public class ExternalizationExample {
 			System.out.println("Price= "+ newCar.getPrice());
 			System.out.println("Is Luxury= "+ newCar.isLuxury());
 			
+			/*
+			 * In this example we can see that the data we are writing by writeExternal() method
+			 * and the data we are reading from readExternal() method must be in the same sequence
+			 * otherwise the data may corrupt or we may get EOFException.
+			 * So, please be very careful while writing and reading data.
+			 */
+			
 		} catch (IOException | ClassNotFoundException e) {
 			e.printStackTrace();
 		}
@@ -113,16 +120,66 @@ class Car implements Externalizable{
 	@Override
 	public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
 		System.out.println("Inside readExternal reading object");
-		this.name = (String)in.readObject();
+		
+		//normal case
+		
+		/*this.name = (String)in.readObject();
 		this.launchYear = in.readInt();
 		this.noOfTires = in.readInt();
 		this.price = in.readFloat();
+		this.isLuxury = in.readBoolean();*/
+		
+		// java.io.EOFException case example
+		
+		/*this.name = (String)in.readObject();
+		this.launchYear = in.readInt();
+		this.noOfTires = in.readInt();
+		this.price = in.readFloat();
+		this.isLuxury = in.readBoolean();*/
+		
+		// corrupted data case here in readExternal() method
+		// we will be trying to readFloat value first where as
+		// in writeExternal() method we have written int value 
+		// first. And this case will give us conclusion that the
+		// data we are writing in writeExternal() and the data
+		// we are reading in the readExternal() must be in the
+		// same sequence.
+		
+		this.name = (String)in.readObject();
+		this.price = in.readFloat();
+		this.launchYear = in.readInt();
+		this.noOfTires = in.readInt();
 		this.isLuxury = in.readBoolean();
 	}
 
 	@Override
 	public void writeExternal(ObjectOutput out) throws IOException {
 		System.out.println("Inside writeExternal writing object");
+		
+		//normal case
+		
+		/*out.writeObject(name);
+		out.writeInt(launchYear);
+		out.writeInt(noOfTires);
+		out.writeFloat(price);
+		out.writeBoolean(isLuxury);*/
+		
+		// java.io.EOFException example here in this case we will be
+		// not writing value for launchYear and will be trying to read it
+		// in the readExternal method
+		
+		/*out.writeObject(name);
+		//out.writeInt(launchYear);
+		out.writeInt(noOfTires);
+		out.writeFloat(price);
+		out.writeBoolean(isLuxury);*/
+		
+		// corrupted data case example here in writeExternal()
+		// we are writing int first where as we will try
+		// to read float value first in readExternal()
+		// and the data will be corrupted.
+		// So, this gives us conclusion that the sequence of data
+		// in readExternal() and writeExternal() must be same.
 		
 		out.writeObject(name);
 		out.writeInt(launchYear);
